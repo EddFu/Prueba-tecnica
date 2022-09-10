39d9ec99ce6a668f8e148e57a560cc26ee190d67
EddFu <esteben305@gmail.com>
2022-09-10 11:00:29 -0500

    se crearon las paginas principales y se añadieron las rutas

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
 =======
 # Prueba-tecnica
 Prueba técnica para angular developer Eduardo Fuentes
->>>>>>> d23a0df97e38b6d9b57f6db08260ed718f93b1b9
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CharacterDetailsComponent } from './pages/character-details/character-details.component';
+import { HomepageComponent } from './pages/homepage/homepage.component';
 
-const routes: Routes = [];
+
+
+const routes: Routes = [
+  {
+    path: '',
+    component: HomepageComponent
+  },
+  {
+    path: 'Character-Details',
+    component: CharacterDetailsComponent
+  },
+  {
+    path: '**',
+    redirectTo: '/'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
