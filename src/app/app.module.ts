01482dc976a344cf1774e9abd527e2283b4703d4
EddFu <esteben305@gmail.com>
2022-09-10 13:14:32 -0500

    se hace el fetch para obtener los datos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomepageComponent } from './pages/homepage/homepage.component';
 import { CharacterDetailsComponent } from './pages/character-details/character-details.component';
+import { HttpClientModule } from '@angular/common/http';
+import { CardComponent } from './components/card/card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomepageComponent,
-    CharacterDetailsComponent
+    CharacterDetailsComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pages/homepage/homepage.component.ts
+++ src/app/pages/homepage/homepage.component.ts
@@ -1,15 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Character } from 'src/app/interface/Character';
+import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-homepage',
   templateUrl: './homepage.component.html',
   styleUrls: ['./homepage.component.css']
 })
-export class HomepageComponent implements OnInit {
+export class HomepageComponent {
 
-  constructor() { }
+  apiUrl = 'https://rickandmortyapi.com/api'
 
-  ngOnInit(): void {
+  characters: Character[] | undefined;
+
+
+  constructor(
+    public http: HttpClient,
+  ) {
+    this.getData();
+   }
+
+   //obtener personajes
+   async getData() {
+    await this.http.get<any>(this.apiUrl + '/character')
+      .subscribe((res) => {
+        console.table(res.results)
+        // console.log(res.results)
+        this.characters = res.results.map(({id, name, image, status, species}: Character) => {
+          return {
+            id: id,
+            name: name,
+            image: image,
+            status: status,
+            species: species,
+          }
+        });
+      });
   }
 
 }
